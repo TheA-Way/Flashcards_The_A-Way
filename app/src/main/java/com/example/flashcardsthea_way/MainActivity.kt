77b17748847caa0d667f2ffb87fe43bfc2ca9af5
TheA-Way <50378035+TheA-Way@users.noreply.github.com>
2022-09-21 01:56:52 -0500

    correction

---
+++ app/src/main/java/com/example/flashcardsthea_way/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.flashcardsthea_way
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val flashcardQuestion = findViewById<TextView>(R.id.Flashcard_Question)
+        val flashcardAnswer = findViewById<TextView>(R.id.Flashcard_Answer)
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+        flashcardAnswer.setOnClickListener {
+            flashcardQuestion.visibility = View.VISIBLE
+            flashcardAnswer.visibility = View.INVISIBLE
+        }
+    }
+}
